06123e00901b1a40e5b39e9fbb4e41fd0f122d2f
Quân Nguyễn Nam <quannguyennam@Quans-MacBook-Pro.local>
2025-02-24 18:53:29 +0700

    Switch to small

--- translate.py
+++ translate.py
@@ -11,7 +11,7 @@ def load_model():
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    model_id = "openai/whisper-large-v3-turbo"
+    model_id = "openai/whisper-small"
 
     st.write("📥 **Loading model...** (This may take some time ⏳)")
     
